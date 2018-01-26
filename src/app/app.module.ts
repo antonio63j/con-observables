e33ca4454364a54ff25259743b2ce0517d04a529
antonio63j <antonio63j@gmail.com>
2018-01-26 13:43:51 +0100

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Sibling1Component } from './sibling/sibling1/sibling1.component';
+import { Sibling2Component } from './sibling/sibling2/sibling2.component';
 
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Sibling1Component,
+    Sibling2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
